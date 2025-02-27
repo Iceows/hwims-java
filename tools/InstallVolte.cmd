9dc902d32c2b066b7b22c01498154a881814844e
Iceows <mounierr07@gmail.com>
2025-02-27 07:59:47 +0100

    Add volte apk script installer

---
+++ tools/InstallVolte.cmd
@@ -0,0 +1,9 @@
+java.exe -jar "ApkSigner.jar" sign  --key platform.pk8 --cert platform.x509.pem  --v4-signing-enabled false --out "HuaweiIMS.apk" "app-debug.apk"
+
+adb root
+adb remount rw, /system
+adb shell mkdir /system/app/HuaweiIMS
+adb push HuaweiIMS.apk /system/app/HuaweiIMS
+adb shell chmod 644 /system/app/HuaweiIMS/HuaweiIMS.apk 
+
+adb reboot
